a3c2e45870b45a7de27249ede87461265cca5527
Faith-Ndanu <faithndanumunyao@gmail.com>
2024-07-12 08:35:06 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar/index.js';
 import LandingPage from './Landinpage/index.js';
+import Resumes from './Resumes';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <LandingPage/>
+      <Resumes/>
       
      
     </div>
